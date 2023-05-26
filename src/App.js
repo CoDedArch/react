9a55be2108f3a3fac45f4a93b1b5f4273fc38657
KELVIN KANYITI GBOLO <kelvingbolo98@gmail.com>
2023-05-26 12:22:54 +0000

    just completed a modal with a form

--- src/App.js
+++ src/App.js
@@ -1,29 +1,57 @@
-import './App.css';
+import './index.css';
 import Employee from './components/Employee';
-import {useState} from 'react'
+import { useState } from 'react';
+import { v4 as uuidV4 } from 'uuid';
+
+
+
 
 function App() {
-  const [role, changeRole] = useState('dev')
+  const [role, changeRole] = useState('dev');
+  const [employess, setEmployees] = useState([
+    {
+      name: 'Kelvin', role: 'CEO manages the company', url: 'https://images.pexels.com/photos/1933873/pexels-photo-1933873.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    },
+    {
+      name: 'Elizabeth', role: role , url: 'https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    },
+    {
+      name: 'Grace', role: 'Mother of the CEO, Counsellor', url: 'https://images.pexels.com/photos/1933873/pexels-photo-1933873.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    },
+    {
+      name: 'Larry', role: 'Gate Man', url: 'https://images.pexels.com/photos/943235/pexels-photo-943235.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    },
+    {
+      name: 'Luke', role: 'CTO', url: 'https://images.pexels.com/photos/428328/pexels-photo-428328.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+    },
+    {
+      name: 'Matta', role: 'Accountant', url: 'https://images.pexels.com/photos/2325622/pexels-photo-2325622.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    },
+  ])
   const showEmployees = true;
 
   const trackInput = event => {
     console.log(event.target.value)
-    changeRole(currentRole => event.target.value)
+    changeRole(event.target.value)
   };
-  console.log("role: "+ role)
-
-
+  console.log("role: " + role)
+  
   return (
-    <div className="App">
+    <div className="App justify-center" >
         {showEmployees?
         <>
           <input type='text' onChange={trackInput} />
 
-          <Employee name="Kelvin" role="CEO manages the company"/>
-          <Employee name='Elizabeth' role={role} />
-          <Employee name='Grace' role="Mother to the CEO. counsellor"/>
-            <Employee name='matta' role="ACCOUNTANT" />
-            <Employee name='Silver Shine' age = {59} status='mr' />
+          <div className='flex flex-wrap'>
+            {
+              employess.map((employee) => <Employee
+                name={employee.name} role={employee.role}
+                img={employee.url}
+                key = {uuidV4}
+              
+              />)
+            }
+          </div>
         </> :
         <h1>Not Allowed to show employess</h1>
         }
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,10 +1,21 @@
+import EditEmployee from "./EditEmployee";
 
 function Employee(props){
   return (
-    <>
-      <h3>Employee {props.name}</h3>
-      <p>{props.role ? props.role: "No Role" }</p>
-    </>
+    <div className="m-2 py-8 px-8 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+      <img className="object-cover h-[100px] w-[100px] block mx-auto rounded-full sm:mx-0 sm:shrink-0" src={props.img} alt="Woman's Face" />
+      <div className="text-center space-y-2 sm:text-left">
+        <div className="space-y-0.5">
+          <p className="text-lg text-black font-semibold">
+            {props.name}
+          </p>
+          <p className="text-slate-400 font-medium">
+            {props.role}
+          </p>
+        </div>
+        <EditEmployee />
+      </div>
+    </div>
 )
 
 }
