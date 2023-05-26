cf838ce3df167a6add995bb587f14760c398fd85
KELVIN KANYITI GBOLO <kelvingbolo98@gmail.com>
2023-05-26 19:12:32 +0000

    concept of paging and props children

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,7 +13,7 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
 
-export default function Header() {
+export default function Header(props) {
   return (
     <Disclosure as="nav" className="bg-gray-800">
       {({ open }) => (
@@ -131,6 +131,7 @@ export default function Header() {
               ))}
             </div>
           </Disclosure.Panel>
+          {props.children}
         </>
       )}
     </Disclosure>
---
+++ src/pages/Employees.js
@@ -0,0 +1,97 @@
+import '../index.css';
+import Employee from '../components/Employee';
+import { useState } from 'react';
+import { v4 as uuidV4 } from 'uuid';
+import AddEmployee from '../components/AddEmployee copy';
+import EditEmployee from '../components/EditEmployee';
+import Header from '../components/Header';
+
+
+function Employees() {
+  const [employees, setEmployees] = useState([
+    {
+      id: 1,
+      name: 'Kelvin', role: 'CEO manages the company', url: 'https://images.pexels.com/photos/1933873/pexels-photo-1933873.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    },
+    {
+      id: 2,
+      name: 'Elizabeth', role: 'dev' , url: 'https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    },
+    {
+      id: 3,
+      name: 'Grace', role: 'Mother of the CEO, Counsellor', url: 'https://images.pexels.com/photos/1933873/pexels-photo-1933873.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    },
+    {
+      id: 4,
+      name: 'Larry', role: 'Gate Man', url: 'https://images.pexels.com/photos/943235/pexels-photo-943235.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    },
+    {
+      id: 5,
+      name: 'Luke', role: 'CTO', url: 'https://images.pexels.com/photos/428328/pexels-photo-428328.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+    },
+    {
+      id: 6,
+      name: 'Matta', role: 'Accountant', url: 'https://images.pexels.com/photos/2325622/pexels-photo-2325622.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    },
+  ])
+  const showEmployees = true;
+
+  function updateEmployee(id, newName, newRole) {
+    const updatedEmployee = employees.map(employee => {
+      if (employee.id === id) {
+        return {
+          ...employee,name:newName,role:newRole
+        }
+      }
+      return employee
+    })
+
+    setEmployees(updatedEmployee)
+  }
+
+  function addEmployee(name, role, imgUrl) {
+    setEmployees(prevEmployees => {
+      return [...prevEmployees, {
+        id: prevEmployees.length + 1,
+        name: name,
+        role: role, 
+        url: imgUrl
+      }]
+    })
+  }
+  
+  return (
+    <div className="App bg-gray-400 min-h-screen" >
+      
+        {showEmployees?
+        <>
+          <div className='flex flex-wrap justify-center my-2'> 
+            {
+              employees.map((employee) => {
+                const editEmployee = <EditEmployee
+                id= {employee.id}
+                name={employee.name}
+                role={employee.role}
+                updateEmployee={updateEmployee} />
+                
+                return (
+                  <Employee
+                    id={employee.id}
+                    name={employee.name} role={employee.role}
+                    img={employee.url}
+                    key={uuidV4}
+                    editEmployee = {editEmployee}
+                  />
+                )
+              })
+            }
+          </div>
+        </> :
+        <h1>Not Allowed to show employees</h1>
+        }
+      <AddEmployee addEmployee = {addEmployee} />
+    </div>
+  );
+}
+
+export default Employees;
